72509c7746c9b90289875dd29026d275392901bf
Hamza <hamza.siddiqui4747@gmail.com>
2026-01-15 08:07:02 +0900

    user models created

--- models/user.go
+++ models/user.go
@@ -1,1 +1,25 @@
 package models
+
+import "time"
+
+type User struct {
+	ID        int       `db:"id"`
+	Email     string    `db:"email"`
+	Password  string    `db:"password"`
+	DOB       time.Time `db:"dob"`
+	Phone     *string   `db:"phone"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
+type UserRegister struct {
+	Email           string  `json:"email"`
+	Password        string  `json:"password"`
+	ConfirmPassword string  `json:"confirm_password"`
+	DOB             string  `json:"dob"`
+	Phone           *string `json:"phone"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
